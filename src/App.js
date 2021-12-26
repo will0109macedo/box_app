b1f38722756574d269f5b3317480e4f58739b863
Will <will.macedo@hotmail.com>
2021-12-25 23:15:44 -0500

    box commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+var adding = 5;
+
+function App() {
+  return (
+    <div className="div">
+    <h3>Link to Google</h3>
+    <a href="https://www.google.com">Google</a>
+    <p>I need a full parragraph here</p>
+    <button>Press Me</button>
+    <input value="Write here" />
+    {adding}
+    </div>
+  );
+}
+
+export default App;
